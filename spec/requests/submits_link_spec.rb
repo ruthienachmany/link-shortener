019f8c47c6b423d183e3edc562996a3572f11ae9
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-11-12 11:05:43 -0500

    slight style fixes

--- spec/requests/submits_link_spec.rb
+++ spec/requests/submits_link_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 require 'factory_girl'
+require 'debugger'
 
 describe "SubmitsLink" do
   it "submits a link to be shortened"
-    link = create(:link)
+    link = FactoryGirl.build(:link)
     visit new_link_path
     fill_in "Link", :with => link.long_link
     click_button "Submit"
   end
-end
+
