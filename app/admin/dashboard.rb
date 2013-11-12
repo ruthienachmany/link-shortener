ccc289d2d2823091e3959ac5793cafb7d7575cf5
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-11-12 02:06:37 -0500

    fixed active admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,24 +1,24 @@
-ActiveAdmin.register_page "Dashboard" do
+# ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+#   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+#   content :title => proc{ I18n.t("active_admin.dashboard") } do
+#     div :class => "blank_slate_container", :id => "dashboard_default_message" do
+#       span :class => "blank_slate" do
+#         span I18n.t("active_admin.dashboard_welcome.welcome")
+#         small I18n.t("active_admin.dashboard_welcome.call_to_action")
+#       end
+#     end
 
-  ActiveAdmin::Dashboards.build do
-    section "Recent Links" do
-      table_for Link.order("created_at desc").limit(5) do
-        column :long_link
-        column :created_at
-      end
-      strong { link_to "View All Links", admin_links_path }
-    end
-  end
-    # end
-  end # content
-end
+#   ActiveAdmin::Dashboards.build do
+#     section "Recent Links" do
+#       table_for Link.order("created_at desc").limit(5) do
+#         column :long_link
+#         column :created_at
+#       end
+#       strong { link_to "View All Links", admin_links_path }
+#     end
+#   end
+#     # end
+#   end # content
+# end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,11 +1,13 @@
 ActiveAdmin.setup do |config|
 
+
   # == Site Title
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
   config.site_title = "Link Shortener"
+
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
